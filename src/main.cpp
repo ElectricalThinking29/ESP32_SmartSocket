a97529c49f322b65dea2eb4e079462db789de612
ElectricalThingking29 <electricalthingking29@gmail.com>
2024-03-02 17:11:01 +0700

    Update code

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,83 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
-// Thu vien thoi gian thuc: https://hocarm.org/dong-ho-thoi-gian-thuc-cho-esp8266/
-// Fix loi khong lay duoc thong tin tu url, thu lam theo: https://arduino.stackexchange.com/questions/33101/use-server-arg-results-as-a-function-parameter
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 // SSID & Password
-const char *ssid = "****";
-const char *password = "****";
+const char *ssid = "Gi cung duoc";
+const char *password = "mKWiF1@#";
 
 WebServer server(80);
 
-const int controlPin = 14; // GPIO14 (D5)
+const int controlPin = 14; // GPIO14 (D14)
 
 String datetime = "YYYY-MM-DDTHH:MM";
 String timezone = "UTC";
 String action = "on";
 
-struct tm timeinfo;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
-// Nội dung HTML & CSS hiển thị trên web server
+bool TimerSet = false;
+
+// HTML content
 String HTML = R"rawliteral(
 <!DOCTYPE html>
 <html>
-  <body>
-    <h1> ESP32 Web Server </h1>   
-    <p> Relay State: %PINSTATE% </p>
-    <button onclick="window.location.href='/relayOn'"> Turn On Socket </button>
-    <button onclick="window.location.href='/relayOff'"> Turn Off Socket </button>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="text-align: center;">
 
-    <h2>Set Relay On/Off Time</h2>
-    <button onclick="window.location.href='/setDateTime'"> Set Timer </button>
-    <button onclick="window.location.href='/cancelDateTime'"> Cancel Timer </button>
-    <p> Scheduled Time: SCHEDULED </p>
-    <form action="/" method="get">
-      <label for="datetime"> Date and time: </label>
-      <input type="datetime-local" id="datetime" name="datetime">
+<h1> ESP32 Web Server </h1>   
+<p> Relay State: %PINSTATE% </p>
+<button onclick="window.location.href='/relayOn'"> Turn On Socket </button>
+<button onclick="window.location.href='/relayOff'"> Turn Off Socket </button>
 
-      <label for="timezone"> Timezone: </label>
-      <select id="timezone" name="timezone">
-        <option value="UTC">UTC</option>
-        <option value="Europe/London">London</option>
-        <option value="America/New_York">New York</option>
-        <option value="Asia/Tokyo">Tokyo</option>
-        <option value="Asia/HochiminhCity">Ho Chi Minh City</option>
-        <!-- other timezones -->
-      </select>
+<h2>Set Relay On/Off Time</h2>
 
-      <label for="action">Action:</label>
-      <select id="action" name="action">
-        <option value="on">Turn On</option>
-        <option value="off">Turn Off</option>
-      </select>
-    </form>
-    
-    <h2>Timer History</h2>
-    <table>
-    <tr> <th>Datetime</th> <th>Timezone</th> <th>Action</th> <th>Set</th> </tr>
-    <tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td></tr>
-    </table>
-  </body>
+<p> Scheduled Time: SCHEDULED </p>
+
+<form action="/setDateTime" method="get">
+
+<label for="datetime"> Date and time: </label>
+<input type="datetime-local" id="datetime" name="datetime">
+
+<label for="timezone"> Timezone: </label>  
+<select id="timezone" name="timezone">
+<option value="UTC">UTC</option>
+<option value="Europe/London">London</option>
+<option value="America/New_York">New York</option>
+<option value="Asia/Tokyo">Tokyo</option>
+<option value="Asia/HochiminhCity">Ho Chi Minh City</option>
+<!-- other timezones -->
+</select>
+
+<label for="action">Action:</label>
+<select id="action" name="action">
+<option value="on">Turn On</option>  
+<option value="off">Turn Off</option>
+</select>
+
+<input type="submit" value="Set">
+<input type="button" value="Cancel" onclick="window.location.href='/cancelDateTime'">
+</form>
+
+<h2>Timer History</h2>
+<table style="width:100%" border="1" cellspacing="0" cellpadding="3">
+<tr> <th>Datetime</th> <th>Timezone</th> <th>Action</th> <th>Set</th> <th></th> </tr>
+<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td> <td></td> </tr> 
+</table>
+
+</body>
 </html>
+
 )rawliteral";
 
 // Xử lý url gốc (/)
 void handle_root()
 {
-  datetime = server.arg("datetime");
-  timezone = server.arg("timezone");
-  action = server.arg("action");
   Serial.print("Datetime: " + datetime);
   Serial.print(", Timezone: " + timezone);
   Serial.println(", Action: " + action);
@@ -82,6 +92,7 @@ void relayOn()
   HTML.replace("%PINSTATE%", "ON");
   HTML.replace("OFF", "ON");
   server.send(200, "text/html", HTML);
+  Serial.println("Relay On");
 }
 
 // Turn relay off
@@ -91,20 +102,34 @@ void relayOff()
   HTML.replace("%PINSTATE%", "OFF");
   HTML.replace("ON", "OFF");
   server.send(200, "text/html", HTML);
+  Serial.println("Relay Off");
 }
 
 // Set relay on/off time
 void setDateTime()
 {
   // Get datetime from url
-  handle_root();
+  datetime = server.arg("datetime");
+  timezone = server.arg("timezone");
+  action = server.arg("action");
+
+  Serial.print("Datetime: " + datetime);
+  Serial.print(", Timezone: " + timezone);
+  Serial.println(", Action: " + action);
 
   HTML.replace("NOT SCHEDULED", "SCHEDULED");
-  HTML.replace("<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td></tr>", "<tr> <td>"+ datetime +"</td> <td>"+ timezone +"</td> <td>" + action + "</td> <td>" + "SET" + "</td> </tr> <tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td></tr>");
+
+  String newRow = "<tr> <td>" + datetime + "</td> <td>" + timezone + "</td> <td>" + action + "</td> <td>" + "SET" + "</td> </tr>" + "<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td> <td></td> </tr> ";
+
+  HTML.replace("<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td> <td></td> </tr> ", newRow);
+
   server.send(200, "text/html", HTML);
+
+  TimerSet = true;
 }
 
-int getSlectedTimezone(String timezone)
+
+int getSlectedTimezone(String &timezone)
 {
   if (timezone == "UTC")
   {
@@ -112,49 +137,77 @@ int getSlectedTimezone(String timezone)
   }
   else if (timezone == "Europe/London")
   {
-    return 3600;
+    return -3600;
   }
   else if (timezone == "America/New_York")
   {
-    return -14400;
+    return +14400;
   }
   else if (timezone == "Asia/Tokyo")
   {
-    return 32400;
+    return -32400;
   }
   else if (timezone == "Asia/HochiminhCity")
   {
-    return 25200;
+    return -25200;
   }
   else
   {
-    return 0;
+    return -1;
   }
+}
+
+// Convert datetime to epoch, seconds from 1st Jan 1970
+unsigned long convertToEpoch(String &datetime, int timezoneOffset)
+{
+  struct tm tm;
+  time_t epochTime;
+  if (datetime.length() > 0)
+  {
+    strptime(datetime.c_str(), "%Y-%m-%dT%H:%M", &tm);
+    epochTime = mktime(&tm);
+  }
+  return epochTime + timezoneOffset;
 }
 
 // Check datetime
 void checkDateTime()
 {
-  // Compare datetime with current time
-  if(!getLocalTime(&timeinfo))
-  {
-    Serial.println("Failed to obtain time");
-    return;
+  timeClient.update();
+
+  // Select timezone
+  int timezoneOffset = getSlectedTimezone(timezone);
+  Serial.println("Timezone: "+String(timezoneOffset));
+  
+  // Get current time
+  unsigned long nowdatetimeEpoch = timeClient.getEpochTime();
+  Serial.println("Now: "+ String(nowdatetimeEpoch));
+  //unsigned long nowdatetime = 1638308400; // 2021-12-03 13:00:00
+  
+  // Convert datetime to epoch
+  unsigned long SetdatetimeEpoch = 0;
+  if(datetime.length() > 0){
+    SetdatetimeEpoch = convertToEpoch(datetime, timezoneOffset);
+  Serial.println("Set: "+ String(SetdatetimeEpoch));
   }
-  String nowdatetime = String(timeinfo.tm_year + 1900) + "-" + String(timeinfo.tm_mon + 1) + "-" + String(timeinfo.tm_mday) + "T" + String(timeinfo.tm_hour) + ":" + String(timeinfo.tm_min);
-  if(action == "on")
+
+  // Compare datetime with current time
+  if (SetdatetimeEpoch == 0)
   {
-    if(nowdatetime == datetime)
+    Serial.println("Failed to convert time");
+    return;
+  } else if (nowdatetimeEpoch == SetdatetimeEpoch)
+  {
+    if (action == "on")
     {
       relayOn();
     }
-  } else if(action == "off")
-  {
-    if(nowdatetime == datetime)
+    else if (action == "off")
     {
       relayOff();
     }
   }
+  delay(1000);
 }
 
 // Cancel relay on/off time
@@ -165,8 +218,10 @@ void cancelDateTime()
   action = "on";
 
   HTML.replace("SCHEDULED", "NOT SCHEDULED");
-  HTML.replace("<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td></tr>", "<tr> <td>"+ datetime +"</td> <td>"+ timezone +"</td> <td>" + action + "</td> <td>" + "RESET" + "</td> </tr> <tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td></tr>");
+  HTML.replace("<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td> <td></td> </tr> ", "<tr> <td> Delete timer </td> <td>"+ timezone +"</td> <td>" + action + "</td> <td>" + "RESET" + "<tr> <td>DATETIME</td> <td>TIMEZONE</td> <td>ACTION</td> <td>SET?</td> <td></td> </tr> ");
   server.send(200, "text/html", HTML);
+
+  TimerSet = false;
 }
 
 void setup()
@@ -197,11 +252,16 @@ void setup()
 
   server.begin();
   Serial.println("HTTP server started");
+
+  timeClient.begin();
   delay(100);
 }
 
 void loop()
 {
   server.handleClient();
-  //checkDateTime();
+  if (TimerSet == true)
+  {
+    checkDateTime();
+  }
 }
